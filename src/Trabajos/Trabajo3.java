41f17261311c24c748f4d56f8849b27d5c811603
Magali Traversi <traversiaylen@gmail.com>
2023-03-21 14:24:23 -0300

    TrabajoUno

---
+++ src/Trabajos/Trabajo3.java
@@ -0,0 +1,10 @@
+package Trabajos;
+
+public class Trabajo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author trave
+ *
+ */
+module ArgentinaPrograma {
+}
